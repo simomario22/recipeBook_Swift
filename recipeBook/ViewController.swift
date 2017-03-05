cfb682f792dcbfa9551a842915f5102258a47f59
michaelgjester <michael.g.jester@gmail.com>
2017-03-05 17:53:23 -0600

    Initial Commit

---
+++ recipeBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  recipeBook
+//
+//  Created by Michael Jester on 3/5/17.
+//  Copyright © 2017 Michael Jester. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
