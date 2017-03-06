ee61af33117bb31b62384d2f636b1375fd20cf29
michaelgjester <michael.g.jester@gmail.com>
2017-03-05 18:43:42 -0600

    create a table view controller inside tab view controller and list recipe names

---
+++ recipeBook/RecipeListViewController.swift
@@ -0,0 +1,87 @@
+//
+//  RecipeListViewController.swift
+//  recipeBook
+//
+//  Created by Michael Jester on 3/5/17.
+//  Copyright © 2017 Michael Jester. All rights reserved.
+//
+
+import UIKit
+
+class RecipeListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var recipeTableView: UITableView!
+    
+    private var recipeArray: [Recipe] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        self.recipeTableView.dataSource = self
+        self.recipeTableView.delegate = self
+        
+        let loadRecipesCompletionHandler: ([Recipe]) -> Void = {[weak self] (recipeArray:[Recipe]) -> Void  in
+            //do something here
+            print("here is my completion handler")
+            
+            for recipe in recipeArray{
+                print("currentRecipe.id = \(recipe.id)")
+                print("currentRecipe.title = \(recipe.title)")
+            }
+            
+            self?.recipeArray = recipeArray
+            self?.recipeTableView.reloadData()
+        }
+        
+        NetworkingManager.loadRecipesWithCompletion(completionHandler:loadRecipesCompletionHandler)
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    // MARK: - 
+    // MARK: UITableView DataSource and Delegate Methods
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipeArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80.0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "RecipeTableViewCellID", for:indexPath)
+        
+        var recipeLabel: UILabel = cell.viewWithTag(99) as! UILabel
+        recipeLabel.text = recipeArray[indexPath.row].title
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("did select a recipe")
+    }
+
+}
